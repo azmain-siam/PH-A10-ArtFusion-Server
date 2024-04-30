250ea3392f786327ab5e5a2fe7390e4f20a6ad99
azmain-siam <siamhossain2700@gmail.com>
2024-04-30 21:01:41 +0600

    Vercel Issue fixed

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
